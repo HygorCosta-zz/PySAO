d9c1cf8c8b5b9efdb12c2011e38b5e4f71ee615a
Hygor Costa <hygorvcosta@gmail.com>
2020-11-05 10:00:33 -0300

    Overall include.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 """ Framework layout to run SAO - Sequential Approximate Optimizaion."""
 from sao_opt.doe import RandomDoE
 from sao_opt.trust_region import TrustRegion
-from sao_opt.opt_problem import OptimizationProblem
+from sao_opt.opt_problem import OptimizationProblem, Simulation
 from sao_opt.surrogate import RadialBasisSurrogate
+from sao_opt.solver import TrustConstrSolver
 from sao_opt.sequence import Sequence
+from sao_opt.converge import Converge
+from sao_opt.results import Results
 
 # ---------- Problem Layer -----------------
 # Create problem
-problem = OptimizationProblem()
+simulation = Simulation()
+problem = OptimizationProblem(simulation)
 
 # Create trust region
-x_init = problem.nominal
+x_init = simulation.nominal
 delta = problem.delta
 bound = problem.bounds
 
@@ -21,16 +25,30 @@ lcons = problem.linear
 
 # ---------- Init Routine Layer ----------------
 # Trust region
-trust_region = TrustRegion(x_init, delta, bound.lb, bound.ub)
-new_lb, new_ub = trust_region.determine_search_region()
+trust_region = TrustRegion(x_init, problem)
 
 # Lhs sample
-doe = RandomDoE(new_lb, new_ub)
+doe = RandomDoE(trust_region.lower, trust_region.upper)
 
 # Evaluate in High fidelity model
-samples_output = problem.high_fidelity(doe.samples)
+samples_output = simulation.high_fidelity(doe.samples)
 
 # Surrogate model
 surrogate = RadialBasisSurrogate(doe.samples, samples_output)
 
+# Optimizer Solver
+solver = TrustConstrSolver(surrogate.evaluate, x_init, bound, lcons)
+
+# Results
+results = Results(simulation, solver, surrogate, trust_region)
+
+# Converge
+converge = Converge(results, problem)
+
+
 # ---------- Sequence Layer -----------------
+sequence = Sequence(simulation, doe, trust_region, surrogate,
+                    solver, converge, results)
+sequence.run()
+
+# ----------- Final Resusts ------------------
