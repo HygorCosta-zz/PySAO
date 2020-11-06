1c7d329aa2b35db6529dcf67cbf7275e88b7561a
Hygor Costa <hygorvcosta@gmail.com>
2020-11-06 16:10:25 -0300

    Test: matrix A and number of cycles.

--- tests/test_opt_problem.py
+++ tests/test_opt_problem.py
@@ -22,15 +22,20 @@ def test_opt_param(problem):
 def test_create_matrix(problem):
     """ Test the linear constraint matrix."""
     matrix = problem._create_matrix()
-    true = [[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]]
-    assert np.array_equal(matrix, true)
+    assert matrix.shape == (4, 24)
+    assert matrix[0][0] == 1
+    assert matrix[0][4] == 0
+    assert matrix[1][0] == 0
+    assert matrix[1][4] == 1
+    assert matrix[2][0] == 0
+    assert matrix[2][12] == 1
+    assert matrix[2][16] == 0
 
 
 def test_get_upper(problem):
     """ Evaluate the upper bound in the linear constraint."""
     eva = problem.get_upper()
-    true = [400/120, 440/79.5]
+    true = [400/120, 440/79.5] * 2
     assert np.array_equal(eva, true)
 
 
@@ -41,7 +46,23 @@ def test_bounds(problem):
     assert np.array_equiv(eva.ub, 12*[1])
 
 
-def test_linear_const(problem):
+def test_linear_create(problem):
     """ Test the linear constraint."""
-    breakpoint()
     assert problem.linear is not None
+
+
+def test_linear_constraint_dot_nominal(problem):
+    """ Verify if 'matrix * x <= upper'."""
+    upper = problem.get_upper()
+    matrix = problem._create_matrix()
+    eva = np.dot(matrix, problem.nominal[0].T)
+    assert all(value <= ubound for value, ubound in zip(eva, upper))
+
+
+def test_linear_constraint_dot_total(problem):
+    """ Verify if 'matrix * x <= upper'."""
+    upper = problem.get_upper()
+    matrix = problem._create_matrix()
+    x_top = np.array([1] * 24)
+    eva = np.dot(matrix, x_top.T)
+    assert all(value >= ubound for value, ubound in zip(eva, upper))
