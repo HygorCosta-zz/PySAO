8929814232551186400681292c17a09a98f41a4b
Hygor Costa <hygorvcosta@gmail.com>
2020-11-08 16:04:47 -0300

    Tests for Results classes - ok.

--- tests/test_results.py
+++ tests/test_results.py
@@ -1,7 +1,7 @@
 """ Test the Results class."""
+from unittest.mock import Mock
 import pytest
 import numpy as np
-from unittest.mock import Mock
 from sao_opt.results import Results
 
 
@@ -10,11 +10,10 @@ def sphere(design_var):
     if not isinstance(design_var, np.ndarray):
         design_var = np.array(design_var)
 
-    resp = []
     if design_var.ndim == 1:
-        value = np.sum(np.power(design_var, 2))
-        resp.append(value)
+        resp = np.sum(np.power(design_var, 2))
     else:
+        resp = []
         for array in design_var:
             value = np.sum(np.power(array, 2))
             resp.append(value)
@@ -27,9 +26,9 @@ def fix_results():
     simulation = Mock()
     simulation.high_fidelity = sphere
     solver = Mock()
-    solver.results.x = [1, 1, 1, 1]
+    solver.result.x = [1, 1, 1, 1]
     solver.x_init = [0.5, 0.5, 0.5, 0.5]
-    solver.results.fun = 0.123
+    solver.result.fun = 0.123
     trust_region = Mock()
     trust_region.delta = 0.2
     trust_region.rho = 0.5
@@ -63,3 +62,48 @@ def test_evaluate_fap_star(results):
     true = 0.123
     eva = results.evaluate_fap_star()
     assert true == eva
+
+
+def test_fobj_list(results):
+    """ Test if the list is created."""
+    fobj_star = sphere([1, 1, 1, 1])
+    fobj_center = sphere([0.5, 0.5, 0.5, 0.5])
+    fap_star = 0.123
+    fobj = [fobj_star, fobj_center, fap_star, fobj_center]
+    eva = results.fobj_list()
+    assert np.array_equal(eva, fobj)
+
+
+def test_update(results):
+    """ Test the update function."""
+    fobj_star = sphere([1, 1, 1, 1])
+    fobj_center = sphere([0.5, 0.5, 0.5, 0.5])
+    fap_star = 0.123
+    results.update()
+    assert results.fob_star == [fobj_star]
+    assert results.fob_center == [fobj_center]
+    assert results.fap_star == [fap_star]
+    assert results.fap_center == [fobj_center]
+    assert np.array_equal(results.x_center, [[0.5, 0.5, 0.5, 0.5]])
+    assert np.array_equal(results.x_star, [[1, 1, 1, 1]])
+    assert np.array_equal(results.delta, [0.2])
+    assert np.array_equal(results.pho, [0.5])
+
+
+def test_update_twice(results):
+    """ Test the update function."""
+    fobj_star = sphere([1, 1, 1, 1])
+    fobj_center = sphere([0.5, 0.5, 0.5, 0.5])
+    fap_star = 0.123
+    results.update()
+    results.update()
+    assert results.fob_star == [fobj_star, fobj_star]
+    assert results.fob_center == [fobj_center, fobj_center]
+    assert results.fap_star == [fap_star, fap_star]
+    assert results.fap_center == [fobj_center, fobj_center]
+    assert np.array_equal(results.x_center, [[0.5, 0.5, 0.5, 0.5],
+                                             [0.5, 0.5, 0.5, 0.5]])
+    assert np.array_equal(results.x_star, [[1, 1, 1, 1],
+                                           [1, 1, 1, 1]])
+    assert np.array_equal(results.delta, [0.2, 0.2])
+    assert np.array_equal(results.pho, [0.5, 0.5])
