732323472e1e03f7c3855d10dc2130fddc55a194
Hygor Costa <hygorvcosta@gmail.com>
2020-11-12 18:52:52 -0300

    Feat: Rbf with polynomial tail

--- sao_opt/surrogate.py
+++ sao_opt/surrogate.py
@@ -1,5 +1,7 @@
 """ Create surrogate model for SAO. """
+import numpy as np
 from scipy.interpolate import Rbf
+from scipy.spatial import distance
 from .doe import RandomDoE
 
 
@@ -7,13 +9,11 @@ class RadialBasisSurrogate():
 
     """Radial Basis Surrogate. """
 
-    def __init__(self, input_train, output_train,
-                 func='cubic'):
-        self._input_vars = input_train
-        self._output_vars = output_train
-        self.option = func
+    def __init__(self):
+        self._input_vars = []
+        self._output_vars = []
+        self.option = None
         self.model = []
-        self.build_model()
 
     @property
     def input_vars(self):
@@ -81,3 +81,111 @@ class RadialBasisSurrogate():
         self.input_vars = doe.samples
         self.output_vars = output
         self.build_model()
+
+
+class RbfPoly:
+
+    """Radial Basis with polynomial tail."""
+
+    def __init__(self):
+        self._input_vars = []
+        self._output_vars = []
+        self.num_samples = []
+        self.dim = []
+        self.lamb = []
+        self.gamma = []
+
+    @property
+    def input_vars(self):
+        """ Getter for input."""
+        return self._input_vars
+
+    @input_vars.setter
+    def input_vars(self, new_input):
+        """ Setter input_vars."""
+        self._input_vars = new_input
+
+    @property
+    def output_vars(self):
+        """ Getter for output."""
+        return self._output_vars
+
+    @output_vars.setter
+    def output_vars(self, new_output):
+        """ Setter for output."""
+        self._output_vars = new_output
+
+    def samples_dim(self):
+        """ Get the number of samples and dimension
+        of the problem."""
+        self.num_samples, self.dim = self.input_vars.shape
+
+    def euclid_distance(self):
+        """ Return the euclidian distance between all samples."""
+        samples = self.input_vars
+        return distance.cdist(samples, samples)
+
+    def get_phi(self):
+        """ Get phi value for cubic RBF."""
+        dist = self.euclid_distance()
+        return np.power(dist, 3)
+
+    def get_p(self):
+        """ Get P matrix."""
+        ones = np.ones((self.num_samples, 1))
+        return np.hstack((self.input_vars, ones))
+
+    def get_a_matrix(self):
+        """ Get matrix A for rbf."""
+        phi = self.get_phi()
+        p_matrix = self.get_p()
+        zeros = np.zeros((self.dim + 1, self.dim + 1))
+        row1 = np.hstack((phi, p_matrix))
+        row2 = np.hstack((p_matrix.T, zeros))
+        return np.vstack((row1, row2))
+
+    def get_rhs(self):
+        """ Get Rhs matrix for rbf."""
+        zeros = np.zeros((self.dim + 1, ))
+        return np.hstack((self.output_vars, zeros))
+
+    def model(self):
+        """ Solve the linear system."""
+        self.samples_dim()
+        matrix_a = self.get_a_matrix()
+        matrix_rhs = self.get_rhs()
+        inv_a = np.linalg.pinv(matrix_a)
+        params = inv_a.dot(matrix_rhs)
+        self.lamb = params[:self.num_samples]
+        self.gamma = params[self.num_samples:]
+
+    def update(self, func, new_lb, new_ub):
+        """ Update the model.
+
+        Parameters
+        ----------
+        func: method
+            Function to evaluate the samples.
+        new_lb: array-like
+            Lower boundaries.
+        new_ub: array-like
+            Upper boundaries.
+        """
+        doe = RandomDoE(new_lb, new_ub)
+        output = func(doe.samples)
+        self.input_vars = doe.samples
+        self.output_vars = output
+        self.model()
+
+    def __call__(self, new_points):
+        """ Predict the value in new_points."""
+        if new_points.ndim == 1:
+            new_points = new_points.reshape(1, -1)
+        dist = distance.cdist(new_points, self.input_vars)
+        nsam = new_points.shape[0]
+        # Cubic RBF
+        phi = np.power(dist, 3)
+        yest_1 = phi.dot(self.lamb)
+        matrix_x = np.hstack((new_points, np.ones((nsam, 1))))
+        yest_2 = matrix_x.dot(self.gamma)
+        return yest_1 + yest_2
